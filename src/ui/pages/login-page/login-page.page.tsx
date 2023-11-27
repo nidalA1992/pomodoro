aac992ac9e18e0153f08ec044c9cac695917188c
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-11-27 21:14:03 +0500

    update auth layout and login page

--- src/ui/pages/login-page/login-page.page.tsx
+++ src/ui/pages/login-page/login-page.page.tsx
@@ -1,17 +1,10 @@
+import { LoginForm } from '../../features/login/login-form';
 import { AuthLayout } from '../../layouts/auth-layout';
-import { Input } from '../../shared/input';
-import { PrimaryButton } from '../../shared/primary-button';
-import { SecondaryButton } from '../../shared/secondary-button';
 
 export const LoginPage = () => {
   return (
     <AuthLayout>
-      AUTH PAGE
-      <Input placeholder='Введите имя' />
-      <PrimaryButton>Зарегестрироваться</PrimaryButton>
-      <PrimaryButton disabled>Зарегестрироваться</PrimaryButton>
-      <SecondaryButton>Зарегестрироваться</SecondaryButton>
-      <SecondaryButton disabled>Зарегестрироваться</SecondaryButton>
+      <LoginForm />
     </AuthLayout>
   );
 };

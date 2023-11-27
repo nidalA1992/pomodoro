d6a47d31ae8a2d00df7a218840e44324b10dde8e
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-11-27 20:08:23 +0500

    add logo in auth layout

--- src/ui/layouts/auth-layout/auth-layout.layout.tsx
+++ src/ui/layouts/auth-layout/auth-layout.layout.tsx
@@ -6,7 +6,18 @@ import s from './auth-layout.module.scss';
 export const AuthLayout: FC<IAuthLayoutProps> = props => {
   return (
     <main className={s.main}>
+      <a href='/' className={s.logo}>
+        <img
+          className={s.logoImg}
+          src='/images/tomato-img.png'
+          height={190}
+          width={190}
+          alt='Логотип pomodoro_box'
+        />
+        <span className={s.logoText}>pomodoro_box</span>
+      </a>
       <div className={s.contentContainer}>{props.children}</div>
+      <p className={s.copy}>Skillbox® | {new Date().getUTCFullYear()}</p>
     </main>
   );
 };

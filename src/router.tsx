9760d0b6601f41b08098d97c610edf4013d42fa1
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-11-27 18:50:59 +0500

    add login page

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { AuthPage } from './ui/pages/auth-page';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>HOME PAGE</h1>,
+    element: <AuthPage />,
     children: [],
   },
   {
     path: '/dashboard',
     element: (
       <div>
-        <h1>MAIN PAGE</h1>
-        <h2>MAIN PAGE</h2>
-        <h3>MAIN PAGE</h3>
-        <h4>MAIN PAGE</h4>
-        <h5>MAIN PAGE</h5>
+        <h1>DASHBOARD</h1>
+        <h2>DASHBOARD</h2>
+        <h3>DASHBOARD</h3>
+        <h4>DASHBOARD</h4>
+        <h5>DASHBOARD</h5>
       </div>
     ),
     children: [],
---
+++ src/ui/pages/login-page/login-page.page.tsx
@@ -0,0 +1,5 @@
+import { AuthLayout } from '../../layouts/auth-layout';
+
+export const LoginPage = () => {
+  return <AuthLayout>AUTH PAGE</AuthLayout>;
+};
